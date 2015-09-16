ecf8b1965d7b47c0456bfdf035dafae65f04f2e9
mattnds <mattnds@gmail.com>
2015-09-16 13:23:30 -0700

    Matthew's edit

---
+++ StudentEmployee.java
@@ -0,0 +1,20 @@
+package salarydemo;
+
+public class StudentEmployee extends Employee {
+
+	private double hourlyWage;
+
+	/**
+	 * 
+	 * @param name
+	 * @param wage
+	 */
+	public void HourlyEmployee(String name, double wage) {
+		
+		setName(name);
+		hourlyWage = wage;
+		this.wage = hours * 9.25 ; 
+	}
+	return wage; 
+
+}
