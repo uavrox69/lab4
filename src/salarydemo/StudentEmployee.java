fa784c64f26835f23e57daab1f409c74d27f64c1
joseph yslas <jyslas@hotmail.com>
2015-09-16 13:29:27 -0700

    new employee sal

---
+++ src/salarydemo/StudentEmployee.java
@@ -0,0 +1,17 @@
+package salarydemo;
+
+public class StudentEmployee extends Employee {
+
+	private double hourlyWage;
+
+	/**
+	 * 
+	 * @param name
+	 * @param wage
+	 */
+	public StudentEmployee(String name, double wage) {
+		// TODO - implement StudentEmployee.StudentEmployee
+		throw new UnsupportedOperationException();
+	}
+
+}
